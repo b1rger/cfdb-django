2023290aefa02f52d4afc5413f35b99862999a78
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2016-07-05 14:15:56 +0200

    add $ to urls.py

--- webpage/urls.py
+++ webpage/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^admin/$', admin.site.urls),
-    url(r'^(?!admin)(?P<template>\w*)', views.template_view, name="template"),
+    url(r'^(?!admin)(?P<template>\w*)$', views.template_view, name="template"),
     url(r'^login/$', views.user_login, name='user_login'),
     url(r'^accounts/login/$', views.user_login, name='user_login'),
     url(r'^logout/$', views.user_logout, name='user_logout'),
