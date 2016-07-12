fc39ca3a7575ab2c2c7ae6f5825e4e40c81100b0
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2016-07-12 15:44:47 +0200

    registerd controlled vocabularies classes in admin backend

--- vocabularies/admin.py
+++ vocabularies/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.register(Region)
+admin.site.register(Archive)
+admin.site.register(Dossier)
+admin.site.register(Scribe)
+admin.site.register(Period)
+admin.site.register(TextType)
 
 # Register your models here.
