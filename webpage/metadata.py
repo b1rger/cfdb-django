78c39e65833acc67c62958849f761973b34520e7
zxenia <ksenia1zaytseva@gmail.com>
2019-01-10 13:56:29 +0100

    project_info added

--- webpage/metadata.py
+++ webpage/metadata.py
@@ -2,8 +2,18 @@
 # (by default) in the base.html and index.html
 
 PROJECT_METADATA = {
-    'title': 'The Projecs Title',
-    'author': 'Author',
-    'subtitle': 'The Projects Subtitle',
-    'description': 'short description of the project',
-    'github': 'http://www.github.com/'}
+    'title': 'LABASI: Late Babylonian Signs',
+    'author': 'Michael Jursa, Reinhard Pirngruber, Matthias Adelhofer, Yuval Levavi, Daniel Schopper, Peter Andorfer, Ksenia Zaytseva',
+    'subtitle': 'LABASI',
+    'description': 'LaBaSi is intended to serve as a research and teaching tool, allowing easy access to the palaeographic\
+     information that is relevant for the study of Babylonian socio-economic texts of the first millennium BCE.',
+    'github': 'https://github.com/acdh-oeaw',
+    'purpose_de': 'Diese Website dient der Information über das Projekt LaBaSi und der Bereitstellung der aus diesem Projekt\
+     hervorgehenden Forschungsdaten und -ergebnisse.',
+    'purpose_en': 'This website provides information on the project LaBaSi as well as the data and results emerging from this project.',
+    'version': '0.0.1',
+    'matomo_id': '34',
+    'matomo_url': '//matomo.acdh.oeaw.ac.at/',
+    'imprint': '/imprint',
+    'app_type': 'database',
+}
--- webpage/urls.py
+++ webpage/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     url(r'^login/$', views.user_login, name='user_login'),
     url(r'^accounts/login/$', views.user_login, name='user_login'),
     url(r'^logout/$', views.user_logout, name='user_logout'),
+     url(r'^project-info/$', views.project_info, name='project_info'),
 ]
